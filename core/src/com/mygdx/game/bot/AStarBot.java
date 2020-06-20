6dbc536119b4275dc876d8729ef22893d443d0e3
aniajj9 <56159175+aniajj9@users.noreply.github.com>
2020-06-20 17:12:25 +0200

    Update AStarBot.java

--- core/src/com/mygdx/game/bot/AStarBot.java
+++ core/src/com/mygdx/game/bot/AStarBot.java
@@ -8,8 +8,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class AStarBot {
-    private static int Height = 640 * 2;
-    private static int Width = 640 * 8 / 3;
     private int[][] nodes;
     private double maxVelocity;
     private double tolerance;
@@ -26,26 +24,12 @@ public class AStarBot {
     }
 
     /**
-     * Method that sets the node table, here we need to set obstacles
-     */
-    public void setNodes() {
-        nodes = new int[Width/partition][Height/partition];
-        for (int i = 0; i < nodes.length; i++) {
-            for (int j = 0; j < nodes[0].length; j++) {
-                nodes[i][j] = 0;
-            }
-        }
-    }
-
-    /**
      * This method puts the work of bot together
      * @param ballNode - node where the ball is
      * @param holeNode - node where the hole is
      * @return an array of vectors that represent the velocity that should be applied to ball one by one
      */
-    public ArrayList<Vector2d> appliedBots(Node ballNode, Node holeNode) {
-        // Set the array
-        setNodes();
+    public ArrayList<Vector2d> appliedBotsHuman(Node ballNode, Node holeNode) {
         ArrayList<Vector2d> moves = new ArrayList<>();
         AStarAlgorithm aStarAlgorithmBot = new AStarAlgorithm(nodes,(int) ballNode.get_x()/partition, (int)ballNode.get_y()/partition,(int)holeNode.get_x()/partition, (int)holeNode.get_y()/partition);
         System.out.println("BALL " + ballNode.x + "  " + ballNode.y);
@@ -85,5 +69,43 @@ public class AStarBot {
             first = new Node(new Node(), (int)foundNodes.get(i).get_x()*partition,(int)foundNodes.get(i).get_y()*partition,foundNodes.get(i).g, foundNodes.get(i).h);
         }
         return moves;
+    }
+    public Vector2d appliedBots(Node ballNode, Node holeNode) {
+        Vector2d firstMove = new Vector2d(0,0);
+        AStarAlgorithm aStarAlgorithmBot = new AStarAlgorithm(nodes,(int) ballNode.get_x()/partition, (int)ballNode.get_y()/partition,(int)holeNode.get_x()/partition, (int)holeNode.get_y()/partition);
+
+        // Compute the path using A* algorithm
+        List<Node> path = aStarAlgorithmBot.wayBetweenNodesFinder();
+
+        // Find lines
+        LineFinder lineFinder = new LineFinder(path);
+        lineFinder.setPointsToLines();
+        List<Integer> lines = lineFinder.getPointsToLines();
+
+        List<Node> foundNodes = lineFinder.getFinalNodes();
+
+        // Find and add velocities to returned list
+        Node first = new Node(new Node(), (int)ballNode.get_x(),(int)ballNode.get_y(),ballNode.g, ballNode.h);
+        for(int i = 0; i<2; i++){
+            System.out.println( "NODE FROM: " + first.get_x() + "   " + first.get_y());
+            Node second = new Node(new Node(), (int)foundNodes.get(i).get_x()*partition,(int)foundNodes.get(i).get_y()*partition,foundNodes.get(i).g, foundNodes.get(i).h);
+            OneShootBot oneShootBot;
+            if (second.get_x() == holeNode.get_x() && second.get_y() == holeNode.get_y()) {
+                oneShootBot = new OneShootBot(maxVelocity, first, second, tolerance, formula, eulerSolver);
+            }
+            else{
+                oneShootBot = new OneShootBot(maxVelocity, first, second, 60, formula, eulerSolver);
+            }
+
+            Vector2d vector2d = oneShootBot.computeVelocity();
+            firstMove.setX(vector2d.get_x());
+            firstMove.setY(vector2d.get_y());
+
+        }
+        return firstMove;
+    }
+
+    public void setNodes(int[][] nodes) {
+        this.nodes = nodes;
     }
 }
